5e779b96b07195bb7283f86eb9e7e283b5228da2
Kunoh85 <kramerterceros@yahoo.com.ar>
2023-01-26 15:21:18 -0300

    desde aph 26/1

---
+++ src/modelos/Concentrado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Concentrado extends Solucion{
+
+    public Concentrado(Integer volumen, Integer concentracion) {
+        super(volumen, concentracion);
+    }
+
+    public Concentrado() {
+    }
+        
+}
